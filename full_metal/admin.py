8edf5e7957890d33b58d84eec98778ca0023a1a5
nightwarriorftw <amanv1999@gmail.com>
2021-08-08 02:30:12 +0530

    Add django-reversions

--- full_metal/admin.py
+++ full_metal/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from full_metal.models import MarketingCampaign
+from full_metal.models import Voucher
 
-# Register your models here.
+from reversion.admin import VersionAdmin
+
+
+@admin.register(MarketingCampaign)
+class MarketingCampaignAdmin(VersionAdmin):
+    pass
+
+@admin.register(Voucher)
+class VoucherAdmin(VersionAdmin):
+    pass
